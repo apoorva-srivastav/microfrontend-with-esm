e67d171438a3a8d0724a2756c7670c53c4c448f6
Apoorva2696 <apoorva2696@gmail.com>
2023-08-16 21:52:15 +0530

    revert

--- custom-library/output1/output.js
+++ custom-library/output1/output.js
@@ -1,21 +1,10 @@
-import * as React from "/Users/aposriva1/Desktop/vite-esm-imports/apps_copy/host-app/node_modules/react/cjs/react.development.js"
-
-
-var Counter = ({ setCount, count }) => {
-  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("button", { onClick: () => setCount(count + 1) }, "Click here"), /* @__PURE__ */ React.createElement("p", null, count));
+// src/components/Button.jsx
+import React from "https://esm.sh/react@18.2.0?dev";
+var BasicButton = ({ text, onClick = () => {
+} }) => {
+  return /* @__PURE__ */ React.createElement("button", { onClick: () => onClick() }, text);
 };
-
-// src/App.jsx
-var App = () => {
-  const [count, setCount] = React.useState(0);
-  return /* @__PURE__ */ React.createElement("section", { className: "app-b" }, /* @__PURE__ */ React.createElement("h3", null, "This is a second remote app"), /* @__PURE__ */ React.createElement(Counter, { count, setCount }));
-};
-var App_default = App;
-
-// src/MicroFrontend.jsx
-var MicroFrontend = () => /* @__PURE__ */ React.createElement(App_default, null);
-var MicroFrontend_default = MicroFrontend;
 export {
-  MicroFrontend_default as default
+  BasicButton
 };
 //# sourceMappingURL=output.js.map
